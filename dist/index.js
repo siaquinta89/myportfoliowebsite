2fe1c7b9efaea172254377c87ba4ff088282fe4a
Stefania <siaquinta89@gmail.com>
2019-05-25 19:18:16 +0200

    about page

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,47 @@
 
-var voices = ['about', 'works', 'contacts'];
+// var voices = ['about', 'works', 'contacts'];
 
 
-//Event Listener click
-document.querySelector('#search-value').addEventListener('search', function(){ 
+// //Event Listener click
+// document.querySelector('#search-value').addEventListener('search', function(){ 
 
-    var input = document.getElementById("search-value").value;
-    var inputUpper = input.toLowerCase();
-        for (i=0; i<voices.length; i++){
-            if (voices[i] == inputUpper) {
-                //Mostro il risultato
-                document.querySelector('.test-' + inputUpper).style.display='block';                
-                return;
-        }
-    }
+//     var input = document.getElementById("search-value").value;
+//     var inputUpper = input.toLowerCase();
+//         for (i=0; i<voices.length; i++){
+//             if (voices[i] == inputUpper) {
+//                 //Mostro il risultato
+//                 document.querySelector('.test-' + inputUpper).style.display='block';                
+//                 return;
+//         }
+//     }
     
-});
+// });
+
+//DROPDOWN MENU
+function myBirthday() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function myCourses() {
+  document.getElementById("myDropdown-2").classList.toggle("show");
+}
+
+function myJobs() {
+  document.getElementById("myDropdown-3").classList.toggle("show");
+}
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+ 
+  
